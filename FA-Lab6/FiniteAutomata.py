e4586427cec4770cb1240315384eff9d27bf2a10
bogdan1809 <50552082+bogdan1809@users.noreply.github.com>
2020-11-11 11:56:31 +0200

    Finished FA, implemented isAccepted function

--- FA-Lab6/FiniteAutomata.py
+++ FA-Lab6/FiniteAutomata.py
@@ -1,4 +1,11 @@
 class FiniteAutomata:
+    def __init__(self):
+        self.Q = None
+        self.E = None
+        self.q0 = None
+        self.F = None
+        self.S = None
+
     def readLine(self, line):
         line = line.strip().split('=')
 
@@ -8,20 +15,23 @@ class FiniteAutomata:
 
     def readFile(self, fileName):
         with open(fileName) as file:
-            Q = self.readLine(file.readline())
-            E = self.readLine(file.readline())
-            q0 = file.readline().split('=')[1].strip()
+            self.Q = self.readLine(file.readline())
+            self.E = self.readLine(file.readline())
+            self.q0 = file.readline().split('=')[1].strip()
 
-            F = self.readLine(file.readline())
+            self.F = self.readLine(file.readline())
 
-            S = []
+            self.S = []
 
             for line in file:
-                self.readTransition(line, S)
-            print(Q, '\n', E, '\n', q0, '\n', F)
-            print(S)
-            print(self.isDeterministic(S))
+                self.readTransition(line, self.S)
 
+    def printAll(self):
+        print(self.Q, '\n', self.E, '\n', self.q0, '\n', self.F)
+        print(self.S)
+
+    def checkIfDeterministic(self):
+        return self.isDeterministic(self.S)
 
     def readTransition(self, line, result):
 
@@ -29,14 +39,36 @@ class FiniteAutomata:
             lhs, rhs = line.split('->')
             newState = rhs.strip()
             oldState, route = [value.strip() for value in lhs.split(',')]
-
-            result.append(((oldState.strip('('), route.strip(')')), newState.strip(',')))
-
+            trans = ((oldState.strip('('), route.strip(')')), newState.strip(', '))
+            if trans not in result:
+                result.append(trans)
+            #result.append(((oldState.strip('('), route.strip(')')), newState.strip(',')))
 
     def isDeterministic(self, transitions):
         lista = []
+        transitionset = set()
         for transition in transitions:
             lista.append(transition[1])
-        if len(set(lista)) == len(lista):
+            transitionset.add(transition[0])
+        if len(set(transitionset)) == len(lista):
             return True
         return False
+
+    def isAccepted(self, sequence):
+        if self.isDeterministic(self.S):
+            state = self.q0
+
+            while (len(sequence) > 0):
+                elem = sequence[0]
+                key = (state, elem)
+                ok = 0
+                for transition in self.S:
+                    if key == transition[0]:
+                        state = transition[1]
+                        sequence = sequence[1:]
+                        ok = 1
+                if ok == 0:
+                    return False
+            if state in self.F:
+                return True
+            return False
--- FA-Lab6/main.py
+++ FA-Lab6/main.py
@@ -3,4 +3,16 @@ from FiniteAutomata import *
 if __name__ == '__main__':
     fa = FiniteAutomata()
     fa.readFile("fa.txt")
-   # print(fa.isDeterministic())
+    opt=100
+    while opt!=0:
+        print("1-Print states,alphabet,transitions and final states")
+        print("2-Verify if FA is deterministic")
+        print("3-Enter sequence and see if accepted")
+        opt=int(input("Pick one"))
+        if opt==1:
+            fa.printAll()
+        elif opt==2:
+            print(fa.checkIfDeterministic())
+        elif opt==3:
+            seq=str(input("enter sequence: "))
+            print(fa.isAccepted(seq))
